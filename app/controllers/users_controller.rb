9e87a71efdbcbc7b912a73af99504e4f310a02ef
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-10-22 12:28:40 +0300

    Add show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,36 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      # session[:user_id] = @user.id
-      redirect_to @user, notice: "Thanks for signing up!"
-    else
-      render :new
+    before_action :require_signin, except: [:new, :create]
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+  
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to @user, notice: "Thanks for signing up!"
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @user = User.find(params[:id])
+    end
+  
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      session[:user_id] = nil
+      redirect_to signin_path
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :email)
     end
   end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
-end
+  
