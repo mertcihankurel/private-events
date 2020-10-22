bcf8b0d143addfb39cafbbd10a0420254ac2ec57
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-10-22 12:42:07 +0300

    Sessign helper and routes for signup and signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
     before_action :require_signin, except: [:new, :create]
-  
+    def index
+        @users = User.all
+    end
     def new
       @user = User.new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
+  root "users#index"
   resources :users, only: [:new, :create, :show]
+  get "signup" => "users#new"
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
 end
