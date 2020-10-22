70f986e0f3291fcac9bfa0c38133614864f57b8a
Amita <sardana.amita2@gmail.com>
2020-10-22 15:21:01 +0530

    Add association for user and event. Add validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :events, foreign_key: :creator_id, dependent: :destroy
+
+  validates :name, presence: true, length: { minimim: 3, maximum: 10 }
+  validates :email, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/ }, uniqueness: true
 end
