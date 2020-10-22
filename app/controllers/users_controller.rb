1eb878e4c4afc344d33927c466ccb60fcf652208
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-10-22 11:52:16 +0300

    Add new create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new(user_params)
+    
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to @user, notice: "Thanks for signing up!"
+        else
+          render :new
+        end
+      end
+    
 end
