d8c448efb0b9d2ae07bfa533b4a44833e7d5afe5
Amita <sardana.amita2@gmail.com>
2020-10-22 17:53:48 +0530

    Add association between event and attendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: :User, foreign_key: :creator_id
+  has_many :event_attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendances, source: :attendee
+  has_one_attached :main_image
 
   scope :past, -> { where("event_date < ?", Time.now).order("event_date DESC") }
   scope :upcoming, -> { where("event_date > ?", Time.now).order("event_date DESC") }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id, dependent: :destroy
+  has_many :event_attendances, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendances, source: :attended_event
 
   validates :name, presence: true, length: { minimim: 3, maximum: 10 }
   validates :email, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/ }, uniqueness: true
