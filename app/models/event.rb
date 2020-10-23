421d8a0c10368827943bcc4907e96829818192d8
Amita <sardana.amita2@gmail.com>
2020-10-23 21:00:02 +0530

    Remove foreign key as suggested in review

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: :User, foreign_key: :creator_id
+  belongs_to :creator, class_name: :User
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances, source: :attendee
   has_one_attached :main_image
